d51770f783f41e89765e8fd4786eba31986b9e03
A.HARIPRIYA <60725880+priya4-hari@users.noreply.github.com>
2021-01-28 10:38:00 +0530

    swapping using third variable and XOR

---
+++ swapping.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, t;
+    printf("enter two numbers :");
+    scanf("%d %d",&a,&b);
+    t=a;
+    a=b;
+    b=t;
+    printf("%d %d",a,b);
+    return 0;
+    }
+#include<stdio.h>
+int main()
+{
+    int a, b;
+    printf("enter two numbers :");
+    scanf("%d %d",&a,&b);
+    a=a^b;
+    b=a^b;
+    a=a^b;
+    printf("%d %d",a,b);
+    return 0;
+    }
